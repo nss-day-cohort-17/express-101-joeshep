7d7047903f3ca409af0b42e5e3d2949635a97d3d
Joe Shepherd <joeshepmedia@gmail.com>
2017-03-27 10:36:31 -0500

    Move routes to separate Router module. Add PORT env variable

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 'use strict';
 
+require('dotenv').config();
 const express = require('express');
 const app = express();
+
+// routes modules
+const animalRoutes = require('./routes/animals');
 
 // middleware
 const requestTime = (req, res, next) => {
@@ -11,30 +15,17 @@ const requestTime = (req, res, next) => {
 
 app.use(express.static(__dirname + '/public'));
 
-app.use(requestTime)
+app.use(requestTime);
 
-app.get('/monkeys', (req, res, next) => {
-  console.log("Fetching some monkeys");
-  console.log(`This ran at ${req.requestedTime}`)
-  res.sendFile(__dirname + '/public/monkeys.html');
-});
-
-app.get('/chickens', (req, res, next) => {
-  console.log("Lookin fer chickens");
-  res.send(`<h3>No chickens for you</h3><form method="POST"><input type="text"><button type="submit">push</button></form>`)
-});
-
-app.post('/chickens', (req, res, next) => {
-  console.log("Posting a form for chickens");
-});
+app.use(animalRoutes);
 
 app.use( (req, res) => {
   res.send("Where do you think you're going? We only have monkeys and chickens here.")
 });
 
+const port = process.env.PORT || 3000
 
-
-app.listen(3000, () => {
-  console.log("Server listening on port 3000");
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 })
 
